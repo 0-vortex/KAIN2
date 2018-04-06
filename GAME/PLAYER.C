fe170c41f80c50e8735acb8fce8ce164d371c8f8
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-04-06 11:04:06 +0100

    Stubs PLAYER.C

---
+++ GAME/PLAYER.C
@@ -0,0 +1,25 @@
+#include "PLAYER.H"
+
+void PLAYER_TurnHead(struct _Instance* instance, short* rotx, short* rotz, struct GameTracker* gameTracker)
+{
+}
+
+long PLAYER_OkToLookAround(struct _Instance* instance)
+{
+}
+
+void PLAYER_SetLookAround(struct _Instance* instance)
+{
+}
+
+void PLAYER_ReSetLookAround(struct _Instance* instance)
+{
+}
+
+long PLAYER_HandleLaunchSignal(struct _Instance* instance, struct Signal* signal)
+{
+}
+
+int PLAYER_ChangeCostume(struct _Instance* instance, struct GameTracker* gameTracker, char* newModelName)
+{
+}
