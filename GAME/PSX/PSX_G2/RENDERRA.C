28b1276b645c1bc3f296b96b1ba1b7d975afd510
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-05-16 10:50:18 +0100

    Add RENDERRA.C
    Add RenderG2_Init
    Add RenderG2_ShutDown

---
+++ GAME/PSX/PSX_G2/RENDERRA.C
@@ -0,0 +1,13 @@
+#include "RENDERRA.H"
+
+#include "G2TYPES.H"
+
+enum _G2Bool_Enum RenderG2_Init(void* appData)
+{
+	return (enum _G2Bool_Enum)1;
+}
+
+void RenderG2_ShutDown(void* appData)
+{
+	return;
+}
