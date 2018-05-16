9a154db0382af08c9f7af38e678f3453f467ccc6
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-05-16 10:50:44 +0100

    Add VIEWVM.C
    Add ViewportG2_Init()
    Add ViewportG2_ShutDown()

---
+++ GAME/PSX/PSX_G2/VIEWVM.C
@@ -0,0 +1,13 @@
+#include "VIEWVM.H"
+
+#include "G2TYPES.H"
+
+enum _G2Bool_Enum ViewportG2_Init(void *appData, unsigned int screenWidth, unsigned int screenHeight)
+{
+	return (enum _G2Bool_Enum)1;
+}
+
+void ViewportG2_ShutDown(void* appData)
+{
+	return;
+}
