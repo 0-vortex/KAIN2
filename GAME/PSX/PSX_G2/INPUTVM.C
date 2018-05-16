c7fc3848a600766e777cefa4f4576a1f17d4a494
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-05-16 10:49:49 +0100

    Add INPUTVM.C
    Add InputG2_Init
    Add InputG2_ShutDown

---
+++ GAME/PSX/PSX_G2/INPUTVM.C
@@ -0,0 +1,13 @@
+#include "INPUTVM.H"
+
+#include "G2TYPES.H"
+
+enum _G2Bool_Enum InputG2_Init(void* appData)
+{
+	return (enum _G2Bool_Enum)1;
+}
+
+void InputG2_ShutDown(void* appData)
+{
+	return;
+}
