50f3d8b8733fe7c3fe70c941db07c02424da5222
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-06-02 18:05:42 +0100

    Add aadStopAllSlots()

--- GAME/PSX/AADSFX.C
+++ GAME/PSX/AADSFX.C
@@ -9,6 +9,27 @@ unsigned short aadStopAllSfx()
 
 unsigned short aadStopAllSlots()
 {
+	struct _AadSequenceSlot* slot;
+	unsigned short slotNumber;
+
+	slotNumber = 0;
+	if (aadMem[0].numSlots != 0)
+	{
+		//loc_80054638
+		for (slotNumber = 0; slotNumber < aadMem->numSlots; slotNumber++)
+		{
+			slot = aadMem[0].sequenceSlots[slotNumber];
+
+			if ((slot->status & 1))
+			{
+				aadStopSlot(slotNumber);
+			}
+			//loc_80054664
+			slot->sequenceNumberAssigned = 255;
+		}
+	}
+	//loc_80054684
+
 	return 0;
 }
 
